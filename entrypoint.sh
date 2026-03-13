4f25b74aab7150a25fe5d5ead0aded186adebf8a
Lorenz Bauer <lorenz.bauer@antithesis.com>
2026-03-13 16:36:19 +0000

    entrypoint: interrupt on SIGTERM
    podman rm -f couldn't terminate the container normally. Use a
    background process + wait.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 trap 'exit 0' SIGTERM SIGINT
 
 while true; do
-    sleep 60
+    sleep 60 & # background so that `wait` can be interrupted by signals
+    wait $!
     session_count=$(ps ax -o pid,tty --no-headers | awk -v me=$$ '$1 != me && $2 ~ /pts/ { count++ } END { print count+0 }')
     if [ "$session_count" -eq 0 ]; then
         exit 0
